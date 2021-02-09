da7e67a2a1af928b6a12d29f4857f64eababa4ef
Gorka Guardiola Múzquiz <paurea@gmail.com>
2021-02-09 20:23:23 +0100

    hash_mutex, better name

--- file.c
+++ file.c
@@ -304,13 +304,13 @@ static int burn_entry(struct file *f, const char __user *buf, size_t count,
 		return -1;
 	}
 	lentry.koffset =  (uint64_t) keyoff;
-	mutex_lock(&sb->hashmutex);	/* careful hash_tfm and hash_desc */
+	mutex_lock(&sb->hash_mutex);	/* careful hash_tfm and hash_desc */
 	if(do_hmac(sb, buf, key, &lentry) < 0){
 		printk(KERN_ERR "sealfs: do_hash failed\n");
-		mutex_unlock(&sb->hashmutex);
+		mutex_unlock(&sb->hash_mutex);
 		return -1;
       	}
-	mutex_unlock(&sb->hashmutex);
+	mutex_unlock(&sb->hash_mutex);
 	sz = sizeof(struct sealfs_logfile_entry);
 	o = sizeof(struct sealfs_logfile_header) + (keyoff/FPR_SIZE)*sz;
 
