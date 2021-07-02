d59b5fa478b5710c3948d9fdf6630d2bc9276cb7
Gorka Guardiola Múzquiz <paurea@gmail.com>
2021-07-02 11:54:05 +0200

    stress.sh

---
+++ tools/stress.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+
+
+ROOTPATH=`realpath $0`
+ROOTPATH=`basename $ROOTPATH `
+ROOTPATH=`realpath $0|sed s,/$ROOTPATH,,`
+
+usage() {
+	echo $0 2>&1
+	exit 1
+}
+
+if [ $# -ne 0 ]; then
+	usage
+fi
+
+TESTNAME=verification
+
+#CREATE ALL THESE
+ssd=/var/tmp/ssd
+once=$HOME/tmp/once
+datadir=$ROOTPATH/data/$TESTNAME
+mkdir -p $datadir
+code=$HOME/gits/sealfs
+
+r=$ssd
+
+wrounds=1000
+ksize=$((1024 * 1024 * 1024))
+
+if mount | grep -q 'type sealfs'
+then
+        echo sealfs is mounted, clean up before running the experiment >&2
+        exit 1
+fi
+
+for d in $ssd/sealfs $once $datadir $code
+do
+	if ! test -d $d
+	then
+		echo $d does not exist, create it >&2
+		exit 1
+	fi
+done
+rm $ssd/sealfs/* $ssd/k[12]  2> /dev/null
+sync; sync
+
+
+if ! test -f $once/k1 || ! test -f $once/.SEALFS.LOG
+then
+        echo prepare first >&2
+	echo run: $code/tools/prep $once/.SEALFS.LOG $once/k1 $once/k2 $ksize >&2
+	exit 1
+fi
+
+badcode () {
+	echo could not make $1 in /tmp/code 2>&1
+	exit 1
+}
+
+rm -rf /tmp/code
+mkdir /tmp/code
+cp -a $code/* /tmp/code
+if test -f ${TESTNAME}_*; then
+	filename=`echo ${TESTNAME}_*|sed s/${TESTNAME}_//g|head`
+fi
+
+(cd /tmp/code; make clean; make) || badcode kernel
+
+(cd $code/tools; make) || badcode tools
+
+runonce(){
+	sudo rmmod sealfs
+	if ! sudo insmod /tmp/code/sealfs.ko
+	then
+	        echo cant load module >&2
+	        exit 1
+	fi
+	rm $r/sealfs/* 2> /dev/null
+	cp $once/.SEALFS.LOG  $r/sealfs/
+	cp $once/k1 $r
+	if ! sudo mount -t sealfs $r/sealfs $r/sealfs -o async,nratchet=$nratchet,kpath=$r/k1
+	  then
+	          echo cant mount >&2
+	          exit 1
+	  fi
+	if ! mount | grep -q "on $r/sealfs type sealfs"
+	then
+	   echo sealfs is not mounted >&2
+	   exit 1
+	fi
+	nprocs=4
+	wsize=100
+	wrounds=250000
+	end=$((wrounds*nprocs*wsize))
+	
+	$code/tools/test -s $nprocs $wsize $wrounds $r/sealfs > /dev/null
+	inode=$(ls -i $r/sealfs|awk '{print $1}')
+	sync; sync
+	sudo umount $r/sealfs
+	sync; sync
+	if sudo $code/tools/verify $r/sealfs $r/k1 $once/k2 >/dev/null 2>&1; then
+		echo OK for $1$nratchet
+	else
+		echo nprocs: $nprocs wsize: $wsize wrounds: $wrounds $r/sealfs 
+		echo FAIL: sudo $code/tools/verify  $r/sealfs $r/k1 $once/k2 2>&1;
+		exit 1
+	fi
+	echo
+}
+
+NRATCHET='1 2 32 64'
+for n in `seq 1 20`; do
+	for nratchet in $NRATCHET; do
+		runonce
+	done
+done
+sudo rmmod sealfs
