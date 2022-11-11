c17b01b0c399a69e9c7deb705b2ddd7180024d6a
Gorka Guardiola Múzquiz <paurea@gmail.com>
2022-11-11 18:06:19 +0100

    prepfiles

--- tools/go/prep/prep.go
+++ tools/go/prep/prep.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"sealfs/sealfs/prepfiles"
 	"crypto/rand"
 	"encoding/binary"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"sealfs/sealfs/headers"
 	"strconv"
 )
 
@@ -18,51 +17,6 @@ func usage() {
 	os.Exit(2)
 }
 
-func createKeyFile(name string, magic uint64) (kf *os.File, err error) {
-	kf, err = os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("cannot create %s: %s\n", name, err)
-	}
-	kh := &headers.KeyFileHeader{Magic: magic, Burnt: 0}
-	if err = kh.WriteHeader(kf); err != nil {
-		return nil, err
-	}
-	return kf, err
-}
-
-func prepKeyFiles(name string, name2 string, size int64, magic uint64) (err error) {
-	var (
-		k1 *os.File
-		k2 *os.File
-	)
-	if k1, err = createKeyFile(name, magic); err != nil {
-		return err
-	}
-	defer k1.Close()
-	if k2, err = createKeyFile(name2, magic); err != nil {
-		return err
-	}
-	defer k2.Close()
-	if size == 0 {
-		log.Printf("warning, 0 size keystream %s\n", name)
-	}
-	if size < 0 {
-		return fmt.Errorf("size too small for keystream %d\n", size)
-	}
-	w := io.MultiWriter(k1, k2)
-	_, err = io.CopyN(w, rand.Reader, size)
-	return err
-}
-
-func createLogFile(name string, magic uint64) error {
-	lf, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		return fmt.Errorf("cannot create %s: %s\n", name, err)
-	}
-	defer lf.Close()
-	lh := &headers.LogFileHeader{Magic: magic}
-	return lh.WriteHeader(lf)
-}
 
 func main() {
 	log.SetPrefix("SealFs: ")
@@ -84,11 +38,11 @@ func main() {
 		fmt.Fprintf(os.Stderr, "magic: %x\n", magic)
 	}
 
-	err = createLogFile(args[1], magic)
+	err = prepfiles.CreateLogFile(args[1], magic)
 	if err != nil {
 		log.Fatalf("cannot create %s: %s\n", args[2], err)
 	}
-	err = prepKeyFiles(args[2], args[3], int64(keysize), magic)
+	err = prepfiles.PrepKeyFiles(args[2], args[3], int64(keysize), magic)
 	if err != nil {
 		log.Fatalf("cannot create %s: %s\n", args[2], err)
 	}
---
+++ tools/go/prepfiles/prepfiles.go
@@ -0,0 +1,56 @@
+package prepfiles
+
+import (
+	"io"
+	"crypto/rand"
+	"log"
+	"os"
+	"sealfs/sealfs/headers"
+	"fmt"
+)
+
+func CreateLogFile(name string, magic uint64) error {
+	lf, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("cannot create %s: %s\n", name, err)
+	}
+	defer lf.Close()
+	lh := &headers.LogFileHeader{Magic: magic}
+	return lh.WriteHeader(lf)
+}
+
+func CreateKeyFile(name string, magic uint64) (kf *os.File, err error) {
+	kf, err = os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("cannot create %s: %s\n", name, err)
+	}
+	kh := &headers.KeyFileHeader{Magic: magic, Burnt: 0}
+	if err = kh.WriteHeader(kf); err != nil {
+		return nil, err
+	}
+	return kf, err
+}
+
+func PrepKeyFiles(name string, name2 string, size int64, magic uint64) (err error) {
+	var (
+		k1 *os.File
+		k2 *os.File
+	)
+	if k1, err = CreateKeyFile(name, magic); err != nil {
+		return err
+	}
+	defer k1.Close()
+	if k2, err = CreateKeyFile(name2, magic); err != nil {
+		return err
+	}
+	defer k2.Close()
+	if size == 0 {
+		log.Printf("warning, 0 size keystream %s\n", name)
+	}
+	if size < 0 {
+		return fmt.Errorf("size too small for keystream %d\n", size)
+	}
+	w := io.MultiWriter(k1, k2)
+	_, err = io.CopyN(w, rand.Reader, size)
+	return err
+}
