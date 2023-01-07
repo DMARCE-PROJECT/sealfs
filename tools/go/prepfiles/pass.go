1daacd1bb3f6098621435ed70e7dd0b52159f2ae
Gorka Guardiola Múzquiz <paurea@gmail.com>
2023-01-07 14:40:15 +0100

    use scrypt instead of pbkdf2

--- tools/go/prepfiles/pass.go
+++ tools/go/prepfiles/pass.go
@@ -7,7 +7,7 @@ import (
 	"crypto/sha256"
 	"encoding/binary"
 	"fmt"
-	"golang.org/x/crypto/pbkdf2"
+	"golang.org/x/crypto/scrypt"
 	"golang.org/x/term"
 	"io"
 	"log"
@@ -23,26 +23,29 @@ type keyReader struct {
 	key    [entries.FprSize]byte
 }
 
-const nIterKeyDeriv = 4096
+const nIterKeyDeriv = 32768 * 4
 
-func NewKeyReader(pass []byte, magic uint64) (keyr *keyReader) {
+func NewKeyReader(pass []byte, magic uint64) (keyr *keyReader, err error) {
 	//magic is the salt
 	salt := make([]byte, 8)
 	binary.LittleEndian.PutUint64(salt, magic)
-	dk := pbkdf2.Key(pass, salt, nIterKeyDeriv, len(keyr.state), sha256.New)
+	dk, err := scrypt.Key(pass, salt, nIterKeyDeriv, 8, 1, len(keyr.state))
+	if err != nil {
+		return nil, err
+	}
 	keyr = &keyReader{}
 	copy(keyr.state[:], dk)
 
 	//delete the password and temporary key
-	_, err := io.ReadFull(rand.Reader, pass)
+	_, err = io.ReadFull(rand.Reader, pass)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 	_, err = io.ReadFull(rand.Reader, dk)
 	if err != nil {
-		return nil
+		return nil, err
 	}
-	return keyr
+	return keyr, nil
 }
 
 func (keyr *keyReader) Ratchet() (err error) {
@@ -104,9 +107,9 @@ func PassPrepKeyFile(name string, size int64, magic uint64) (err error) {
 	if size < 0 {
 		return fmt.Errorf("size too small for keystream %d", size)
 	}
-	keyreader := NewKeyReader(pass, magic)
+	keyreader, err := NewKeyReader(pass, magic)
 	if keyreader == nil {
-		return fmt.Errorf("cannot create keyreader")
+		return fmt.Errorf("cannot create keyreader: %s", err)
 	}
 	w := bufio.NewWriter(k1)
 	_, err = io.CopyN(w, keyreader, size)
