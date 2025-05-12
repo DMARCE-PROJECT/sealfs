7c34597ccb250f92a2118d9a0fba561474cc0e04
Gorka Guardiola Múzquiz <gorka.guardiola@gmail.com>
2025-05-12 18:52:52 +0200

    make sure we put pages on error

--- module/file.c
+++ module/file.c
@@ -242,7 +242,6 @@ static int hash_userbuf( struct sealfs_hmac_state *hmacstate, const char __user 
 			printk(KERN_CRIT "sealfs: get_user_pages, no pages for hashing: %lld res %lld\n", npages, res);
 			return -1;
 		}
-	      	/* could recover, but something is probably up */
 		if(res < npages){
 			npages = res;
 			nbatch = npages*PAGE_SIZE-offset;
@@ -268,6 +267,9 @@ static int hash_userbuf( struct sealfs_hmac_state *hmacstate, const char __user 
 			nb -= nhash;
 			if(nb <= 0)
 				break;
+		}
+		for(np=np+1; np < npages; np++){
+			put_page(pages[np]);
 		}
 		if(rerr){
 			return -1;
